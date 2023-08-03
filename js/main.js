699c16093175d53d99f07c4ce859bd886e5e9f12
insertusernamed <113151220+insertusernamed@users.noreply.github.com>
2023-08-03 15:58:22 -0400

    button qol and checkbox saving

--- js/main.js
+++ js/main.js
@@ -6,109 +6,132 @@ Jenna Deamer - 200529678
 //--------------------------// Local storage functions //--------------------------//
 // Function to save the todo list to localStorage
 function saveTodoList() {
-  const todoList = document.getElementById('todoList').innerHTML;
-  const title = document.querySelector('h1').textContent;
+    const todoList = document.getElementById("todoList").innerHTML;
+    //window.alert(todoList);
+    const title = document.querySelector("h1").textContent;
 
-  localStorage.setItem('todoList', todoList);
-  localStorage.setItem('title', title);
+    localStorage.setItem("todoList", todoList);
+    localStorage.setItem("title", title);
 }
 
 // Function to load the todo list from localStorage
 function loadTodoList() {
-  const todoList = localStorage.getItem('todoList');
-  const title = localStorage.getItem('title');
+    const todoList = localStorage.getItem("todoList");
+    const title = localStorage.getItem("title");
 
-  if (todoList) {
-    document.getElementById('todoList').innerHTML = todoList;
-  }
+    if (todoList) {
+        document.getElementById("todoList").innerHTML = todoList;
+        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach(function (checkbox) {
+            const listItem = checkbox.nextElementSibling;
+            if (listItem.classList.contains("completed")) {
+                checkbox.checked = !checkbox.checked;
+            }
+        });
+    }
 
-  if (title) {
-    document.querySelector('h1').textContent = title;
-    toggleTitleStyle(); // Apply italic style if needed
-  }
+    if (title) {
+        document.querySelector("h1").textContent = title;
+        //toggleTitleStyle(); // Apply italic style if needed
+    }
 }
 
 // Load the todo list from localStorage when the page loads
-document.addEventListener('DOMContentLoaded', function () {
-  loadTodoList();
+document.addEventListener("DOMContentLoaded", function () {
+    loadTodoList();
 
-  // Add event listener for "input" event on the title to handle multiline prevention
-  const titleElement = document.querySelector('h1[contenteditable="true"]');
-  titleElement.addEventListener('input', function (event) {
-    // Replace any newline characters with an empty string to prevent multiline
-    this.textContent = this.textContent.replace(/\n/g, '');
-  });
+    // Add event listener for "input" event on the title to handle multiline prevention
+    const titleElement = document.querySelector('h1[contenteditable="true"]');
+    titleElement.addEventListener("input", function (event) {
+        // Replace any newline characters with an empty string to prevent multiline
+        this.textContent = this.textContent.replace(/\n/g, "");
+    });
 
-  // Add event listener for "blur" event on the title to remove focus and selection
-  titleElement.addEventListener('blur', function (event) {
-    window.getSelection().removeAllRanges(); // Remove text selection highlight
-    this.blur(); // Remove focus from the title element
-  });
+    // Add event listener for "blur" event on the title to remove focus and selection
+    titleElement.addEventListener("blur", function (event) {
+        window.getSelection().removeAllRanges(); // Remove text selection highlight
+        this.blur(); // Remove focus from the title element
+    });
 });
 
+//--------------------------// Todolist functions //--------------------------//
 function addTodo() {
-  const todoText = document.getElementById('newTodo').value;
-  if (!todoText) return;
+    const todoText = document.getElementById("newTodo").value;
+    if (!todoText) return;
 
-  const todoList = document.getElementById('todoList');
-  const todoItem = document.createElement('li');
-  todoItem.className = 'todo-item';
-  todoItem.innerHTML = `
+    const todoList = document.getElementById("todoList");
+    const todoItem = document.createElement("li");
+    todoItem.className = "todo-item";
+    todoItem.innerHTML = `
       <input type="checkbox" onchange="toggleCompleted(this)">
       <span>${todoText}</span>
       <button onclick="deleteTodo(this)" class="delete-btn">Delete</button>
     `;
-  todoList.appendChild(todoItem);
-  document.getElementById('newTodo').value = '';
+    todoList.appendChild(todoItem);
+    document.getElementById("newTodo").value = "";
 
-  saveTodoList();
+    saveTodoList();
 }
 
 function toggleCompleted(checkbox) {
-  const listItem = checkbox.closest('li');
-  const ul = listItem.parentNode;
+    const listItem = checkbox.closest("li");
+    const ul = listItem.parentNode;
 
-  if (checkbox.checked) {
-    listItem.querySelector('span').classList.add('completed');
-    ul.appendChild(listItem);  // Move checked item to the bottom
-  } else {
-    listItem.querySelector('span').classList.remove('completed');
-    ul.insertBefore(listItem, ul.firstChild);  // Move unchecked item to the top
-  }
+    if (checkbox.checked) {
+        listItem.querySelector("span").classList.add("completed");
+        ul.appendChild(listItem); // Move checked item to the bottom
+    } else {
+        listItem.querySelector("span").classList.remove("completed");
+        ul.insertBefore(listItem, ul.firstChild); // Move unchecked item to the top
+    }
+    saveTodoList();
 }
 
-// Add event listener for checkbox change event
-document.addEventListener('change', function (event) {
-  const checkbox = event.target;
-  if (checkbox.type === 'checkbox') {
-    const id = checkbox.parentNode.id;
-    checkboxStates[id] = checkbox.checked;
-    saveTodoList();
-  }
+function deleteTodo(button) {
+    const todoItem = button.parentNode;
+
+    if (!button.classList.contains("confirm")) {
+        // First click, change the button text to "Confirm?"
+        button.textContent = "Confirm?";
+        button.classList.add("confirm");
+    } else {
+        // Second click, proceed with deletion
+        const todoList = document.getElementById("todoList");
+
+        // Add the 'deleting' class to initiate the fade-out animation
+        todoItem.classList.add("deleting");
+
+        // After the animation finishes, remove the todo item from the list
+        todoItem.addEventListener("animationend", function () {
+            todoList.removeChild(todoItem);
+
+            // Save the updated todo list to localStorage
+            saveTodoList();
+        });
+    }
+}
+
+// Function to reset the delete button state
+function resetDeleteButtonState() {
+    const confirmButtons = document.querySelectorAll(".confirm");
+    confirmButtons.forEach((button) => {
+        button.textContent = "Delete";
+        button.classList.remove("confirm");
+    });
+}
+
+// Add a click event listener to the document to reset the delete button state
+document.addEventListener("click", function (event) {
+    const target = event.target;
+    if (!target.classList.contains("delete-btn")) {
+        resetDeleteButtonState();
+    }
 });
 
-function deleteTodo(button) {
-  const todoItem = button.parentNode;
-
-  if (!todoItem.classList.contains('confirm')) {
-    // First click, change the button text to "Confirm?"
-    button.textContent = 'Confirm?';
-    todoItem.classList.add('confirm');
-  } else {
-    // Second click, proceed with deletion
-    const todoList = document.getElementById('todoList');
-
-    // Add the 'deleting' class to initiate the fade-out animation
-    todoItem.classList.add('deleting');
-
-    // After the animation finishes, remove the todo item from the list
-    todoItem.addEventListener('animationend', function () {
-      todoList.removeChild(todoItem);
-
-      // Save the updated todo list to localStorage
-      saveTodoList();
-    });
-  }
+function handleKeyDown(event) {
+    if (event.key === "Enter") {
+        addTodo();
+    }
 }
 
 // idk why but this makes the h1 text write from right to left??? who needs italics anyway
@@ -123,24 +146,25 @@ function deleteTodo(button) {
 //   saveTodoList();
 // }
 
-document.addEventListener('DOMContentLoaded', function () {
-  const h1Element = document.querySelector('h1');
+document.addEventListener("DOMContentLoaded", function () {
+    const h1Element = document.querySelector("h1");
 
-  // Add event listener to the <h1> element to handle focus and blur
-  h1Element.addEventListener('focus', function () {
-    if (h1Element.textContent.trim() === 'Untitled To-Do List') {
-      h1Element.textContent = '';
-    }
-    h1Element.style.fontStyle = 'normal'; // Remove italic style when editing starts
-  });
+    // Add event listener to the <h1> element to handle focus and blur
+    h1Element.addEventListener("focus", function () {
+        if (h1Element.textContent.trim() === "Untitled To-Do List") {
+            h1Element.textContent = "";
+        }
+        saveTodoList();
+    });
 
-  h1Element.addEventListener('blur', function () {
-    if (h1Element.textContent.trim() === '') {
-      h1Element.textContent = 'Untitled To-Do List';
-    }
-    toggleTitleStyle(); // Apply italic style if needed
-  });
+    h1Element.addEventListener("blur", function () {
+        if (h1Element.textContent.trim() === "") {
+            h1Element.textContent = "Untitled To-Do List";
+        }
+        saveTodoList();
+        //toggleTitleStyle(); // Apply italic style if needed
+    });
 
-  // Call toggleTitleStyle initially to apply the correct style
-  toggleTitleStyle();
+    // Call toggleTitleStyle initially to apply the correct style
+    //toggleTitleStyle();
 });
